5ca33059cf71fbeaa1bc7343ebe2345a1636584c
Pranay0083 <0083pranay@gmail.com>
2025-10-04 19:34:53 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-400'>
+      <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="bg-red-400" alt="Vite logo" />
+          <img src="" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,7 +19,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
